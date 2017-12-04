5d45d8f0696ef7b5b0ca724b724e4c1218125c18
marvin <mengfeng@chipwing.com>
2017-12-04 15:47:14 +0800

    feat(cart): vue实例及列表信息

---
+++ js/cart.js
@@ -0,0 +1,27 @@
+/**
+ * Created by marvin on 17/12/4.
+ */
+
+var vm = new Vue({
+  el: '#app',
+  data: {
+    totalMoney: 0,
+    productionList: ''
+  },
+  filter: {},
+  mounted: function () {
+    this.$nextTick(() => {
+      this.cartView();
+    })
+  },
+  methods: {
+    cartView: function () {
+      this.$http
+        .get('data/cartData.json')
+        .then(res => {
+          this.productionList = res.data.result.list;
+          this.totalMoney = res.data.result.totalMoney;
+        })
+    }
+  }
+});
