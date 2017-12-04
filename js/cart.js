37ddd5c20c01e402c87309fceb9c210f5b739b3f
marvin <mengfeng@chipwing.com>
2017-12-04 15:52:48 +0800

    feat(cart): 实现加减数量信息

--- js/cart.js
+++ js/cart.js
@@ -22,6 +22,17 @@ var vm = new Vue({
           this.productionList = res.data.result.list;
           this.totalMoney = res.data.result.totalMoney;
         })
+    },
+    changeQuantity: (item, bool) => {
+      // bool为true则为点击"+", 为false则为点击"-"
+      if (bool) {
+        item.productQuantity++;
+      } else {
+        // 最小减到quantity为1
+        if (item.productQuantity > 1) {
+          item.productQuantity--;
+        }
+      }
     }
   }
 });
